2b27525e4ff02107bf1b8ece91884079562475e4
frankr27 <frank.rb27@hotmail.com>
2018-02-16 23:11:14 -0500

    Creación proyecto ERP

---
+++ src/main/java/com/toures/balon/erp/ERPSystem/ERPSystemApplication.java
@@ -0,0 +1,12 @@
+package com.toures.balon.erp.ERPSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ERPSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ERPSystemApplication.class, args);
+	}
+}
